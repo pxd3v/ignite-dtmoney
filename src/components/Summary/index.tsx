8a797383b9d92289531dfbd7008fd46287568670
Pedro Lages <pedro.slages2@gmail.com>
2021-03-16 23:22:21 -0300

    SETUP PROJECT - setup project - create header - create summary - create dashboard - create transactiontable

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import IncomeImg from '../../assets/income.svg';
+import OutcomeImg from '../../assets/outcome.svg';
+import TotalImg from '../../assets/total.svg';
+
+import { Container } from './styles'
+
+export function Summary () {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={IncomeImg} alt=""/>
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={OutcomeImg} alt=""/>
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Entradas</p>
+                    <img src={TotalImg} alt=""/>
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+        </Container>
+    );
+}
